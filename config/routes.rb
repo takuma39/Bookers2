c4559ad07dd2915d7da0d08bb4240a76a9865930
vagrant <vagrant@localhost.localdomain>
2020-05-19 16:22:26 +0000

    Add Bookコントローラ作成

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   root 'homes#top'
   get 'home/about' => 'homes#about'
 
+  resources :books, only: [:new, :create, :index, :show]
+
   devise_for :users
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
