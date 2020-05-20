22e025f9efaf3a9dbfcfe3e36c0631f829893692
vagrant <vagrant@localhost.localdomain>
2020-05-20 17:14:28 +0000

    Add プロフィール画像投稿機能　編集機能　等

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,10 +1,26 @@
 class BooksController < ApplicationController
 	def new
+		@book = Book.new
     end
     def create
+    	@book = Book.new(book_params)
+    	@book.user_id = current_user.id
+    	if @book.save
+    	   redirect_to books_path
+        else
+            render :show
+        end
     end
     def index
+    	@books = Book.all.page(params[:page]).reverse_order
     end
     def show
+    	@post_image = PostImage.find(params[:id])
+        @user = User.find(params[:id])
+    end
+
+    private
+    def user_params
+        params.require(:user).permit(:name, :image, :introduction)
     end
 end
