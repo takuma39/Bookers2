072ae3668c8e92493c86c33e1dbd7d91f6a05211
vagrant <vagrant@localhost.localdomain>
2020-05-21 07:04:02 +0000

    Fix sign in/out後の設定　＆　sidebar作成途中

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,6 +2,7 @@ class BooksController < ApplicationController
 	def new
 		@book = Book.new
     end
+
     def create
     	@book = Book.new(book_params)
     	@book.user_id = current_user.id
@@ -11,16 +12,21 @@ class BooksController < ApplicationController
             render :show
         end
     end
+
     def index
-    	@books = Book.all.page(params[:page]).reverse_order
+
+
     end
+
     def show
     	@post_image = PostImage.find(params[:id])
         @user = User.find(params[:id])
     end
 
     private
-    def user_params
-        params.require(:user).permit(:name, :image, :introduction)
+    def book_params
+        params.require(:book).permit(:title, :body, :user_id)
     end
 end
+
+# @books = Book.all.page(params[:page]).reverse_order
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
   def show
   	@user = User.find(params[:id])
+
   end
 
   def edit
@@ -17,6 +18,11 @@ class UsersController < ApplicationController
   end
 
   private
-def user_params
-    params.require(:user).permit(:name, :profile_image)
+  def user_params
+    params.require(:user).permit(:name, :profile_image, :introduction)
+  end
+
 end
+
+
+# @post_image = PostImage.find(params[:id])
