10056a844224318fb55c30760f9b85a710be6975
vagrant <vagrant@localhost.localdomain>
2020-05-22 07:06:43 +0000

    Add サクセスメッセージ

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::Base
     end
 
     def after_sign_out_path_for(resource)
-    new_user_session_path # ログアウト後に遷移するpathを設定
+    root_path # ログアウト後に遷移するpathを設定
     end
 
 	protected
--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -17,6 +17,7 @@ class BooksController < ApplicationController
     	@book = Book.new(book_params)
     	@book.user_id = current_user.id
     	if @book.save
+           flash[:notice] = "You have creatad book successfully."
     	   redirect_to book_path(@book)
         else
             render :show
@@ -30,8 +31,12 @@ class BooksController < ApplicationController
 
     def update
         @book = Book.find(params[:id])
-        @book.update(book_params)
-        redirect_to book_path(@book.id)
+        if @book.update(book_params)
+           flash[:notice] = "You have updated book successfully."
+           redirect_to book_path(@book.id)
+       else
+           render :edit
+       end
     end
 
     def destroy
